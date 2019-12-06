ef3e97a6df074ec3ea8720798d2ae2e45229ef02
Tom Hart <tom.hart.221@gmail.com>
2019-12-06 11:32:58 +0000

    Apply fixes from StyleCI
    [ci skip] [skip ci]

--- src/Traits/BuildRouteTrait.php
+++ src/Traits/BuildRouteTrait.php
@@ -6,17 +6,17 @@ use InvalidArgumentException;
 
 trait BuildRouteTrait
 {
-
-
     /**
      * Builds a route, providing a routeName property exists on the class.
+     *
      * @param mixed[] $data Static data to be passed.
+     *
      * @return string
      */
     public function buildRoute(array $data = []): string
     {
         if (!property_exists($this, 'routeName')) {
-            throw new InvalidArgumentException(BuildRouteTrait::class . ' requires a "routeName" property');
+            throw new InvalidArgumentException(BuildRouteTrait::class.' requires a "routeName" property');
         }
 
         return route_from_model($this->routeName, $this, $data);
